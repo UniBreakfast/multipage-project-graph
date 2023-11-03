cb4bf926ccdde475b006591b89e8fd30b1f4ac55
UniBreakfast <unibreakfast1@gmail.com>
2023-11-03 22:29:28 +0200

    Delete nodes and update UI

--- pages/nodes/nodes.js
+++ pages/nodes/nodes.js
@@ -1,8 +1,96 @@
 const nodesForm = document.getElementById('nodes-form')
 const nodesSelect = nodesForm.nodes
 const nodeOptionTemplate = nodesSelect.querySelector('template')
+const details = document.getElementById('nodes-details')
+const singleFeedback = details.previousElementSibling
+const firstFeedback = details.querySelector('b')
+const restOfLines = details.querySelector('ul')
+const feedbackTemplate = details.querySelector('template')
 
 getNodes().then(fillNodesSelect)
+
+nodesForm.onsubmit = handleNodesFormSubmit
+
+function handleNodesFormSubmit(e) {
+  const button = e.submitter
+  const action = button.value
+  const { selectedOptions } = nodesSelect
+  const nodeIds = Array.from(selectedOptions).map(option => option.value)
+
+  if (action === 'delete') {
+    deleteNodes(nodeIds)
+  }
+}
+
+async function deleteNodes(nodeIds) {
+  const init = {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(nodeIds),
+  }
+  const result = await fetch('/api/nodes', init)
+
+  handleFirstFeedback()
+
+  if (result.ok) {
+    const nodes = await result.json()
+
+    nodes.reverse()
+
+    nodes.forEach(node => {
+      tellNode(node)
+      removeNodeOption(node)
+    })
+  } else {
+    const problem = await result.json()
+
+    tellProblem(problem.complaint)
+  }
+}
+
+function handleFirstFeedback() {
+  if (details.hidden) {
+    details.hidden = false
+    singleFeedback.hidden = true
+    firstFeedback.replaceChildren(...singleFeedback.childNodes)
+    restOfLines.innerHTML = ''
+  }
+}
+
+function tellNode(node) {
+  const { li, btn, a } = useFeedbackTemplate()
+  const children = [...li.childNodes]
+
+  btn.textContent = node.id
+  a.textContent = node.name
+
+  li.replaceChildren(...firstFeedback.childNodes)
+  restOfLines.prepend(li)
+  firstFeedback.replaceChildren(...children)
+}
+
+function removeNodeOption(node) {
+  const option = nodesSelect.querySelector(`option[value="${node.id}"]`)
+
+  option?.remove()
+}
+
+function tellProblem(problem) {
+  const { li } = useFeedbackTemplate()
+
+  li.replaceChildren(...firstFeedback.childNodes)
+  restOfLines.prepend(li)
+  firstFeedback.textContent = problem.complaint
+}
+
+function useFeedbackTemplate() {
+  const { content } = feedbackTemplate
+  const li = content.firstElementChild.cloneNode(true)
+  const btn = li.firstElementChild
+  const a = btn.nextElementSibling
+
+  return { li, btn, a }
+}
 
 function getNodes() {
   return fetch('/api/nodes')
@@ -14,7 +102,7 @@ function fillNodesSelect(nodes) {
 }
 
 function makeNodeOption(node) {
-  const {option} = useNodeOptionTemplate()
+  const { option } = useNodeOptionTemplate()
 
   option.value = node.id
   option.textContent = node.name
@@ -23,8 +111,8 @@ function makeNodeOption(node) {
 }
 
 function useNodeOptionTemplate() {
-  const {content} = nodeOptionTemplate
+  const { content } = nodeOptionTemplate
   const option = content.firstElementChild.cloneNode(true)
 
-  return {option}
+  return { option }
 }
