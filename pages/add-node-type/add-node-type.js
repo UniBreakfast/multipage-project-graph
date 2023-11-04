f7e3f03888e5904ddaccea0fa097683aac69b9d5
UniBreakfast <unibreakfast1@gmail.com>
2023-11-04 09:12:07 +0200

    Add functionality to handle add node type form submission and display feedback

---
+++ pages/add-node-type/add-node-type.js
@@ -0,0 +1,69 @@
+const addNodeTypeForm = document.getElementById('add-node-type-form')
+const details = document.getElementById('add-node-type-details')
+const singleFeedback = details.previousElementSibling
+const firstFeedback = details.querySelector('b')
+const restOfLines = details.querySelector('ul')
+const feedbackTemplate = details.querySelector('template')
+
+addNodeTypeForm.onsubmit = handleAddNodeTypeSubmit
+
+async function handleAddNodeTypeSubmit() {
+  const payload = Object.fromEntries(new FormData(addNodeTypeForm))
+  const init = {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(payload),
+  }
+  const result = await fetch('/api/node-types', init)
+
+  handleFirstFeedback()
+
+  if (result.ok) {
+    const nodeType = await result.json()
+
+    tellNodeType(nodeType)
+    addNodeTypeForm.reset()
+  } else {
+    const problem = await result.json()
+
+    tellProblem(problem.complaint)
+  }
+}
+
+function handleFirstFeedback() {
+  if (details.hidden) {
+    details.hidden = false
+    singleFeedback.hidden = true
+    firstFeedback.replaceChildren(...singleFeedback.childNodes)
+    restOfLines.innerHTML = ''
+  }
+}
+
+function tellNodeType(nodeType) {
+  const { li, btn, a } = useFeedbackTemplate()
+  const children = [...li.childNodes]
+
+  btn.textContent = nodeType.id
+  a.textContent = nodeType.type
+  a.href += nodeType.id
+
+  li.replaceChildren(...firstFeedback.childNodes)
+  restOfLines.prepend(li)
+  firstFeedback.replaceChildren(...children)
+}
+
+function tellProblem(problem) {
+  const { li } = useFeedbackTemplate()
+
+  li.replaceChildren(...firstFeedback.childNodes)
+  restOfLines.prepend(li)
+  firstFeedback.textContent = problem.complaint
+}
+
+function useFeedbackTemplate() {
+  const li = feedbackTemplate.content.cloneNode(true).querySelector('li')
+  const btn = li.querySelector('button')
+  const a = li.querySelector('a')
+
+  return { li, btn, a }
+}
