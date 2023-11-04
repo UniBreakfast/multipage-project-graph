0cc6d31ebf5b29774e3c856b0f6f168509093993
UniBreakfast <unibreakfast1@gmail.com>
2023-11-04 22:54:36 +0200

    Add node selection functionality to node form

--- pages/node/node.js
+++ pages/node/node.js
@@ -1,4 +1,6 @@
 const nodeForm = document.getElementById('node-form')
+const nodeSelect = nodeForm.id
+const nodeOptionTemplate = nodeSelect.querySelector('template')
 const nodeTypesSelect = nodeForm.type
 const nodeTypeOptionTemplate = nodeTypesSelect.querySelector('template')
 const details = document.getElementById('node-details')
@@ -8,11 +10,29 @@ const restOfLines = details.querySelector('ul')
 const feedbackTemplate = details.querySelector('template')
 
 getNodeTypes().then(fillNodeTypesSelect)
+  .then(getNodes)
+  .then(fillNodeSelect)
   .then(getCurrentNodeId)
   .then(getNode)
   .then(actualizeNodeForm)
 
+window.onhashchange = handleHashChange
+
+nodeSelect.onchange = handleSelectNode
+
 nodeForm.onsubmit = handleNodeFormSubmit
+
+function handleHashChange() {
+  getCurrentNodeId().then(getNode).then(actualizeNodeForm)
+}
+
+function handleSelectNode(e) {
+  const id = nodeSelect.value
+
+  nodeSelect.setAttribute('value', id)
+  location.hash = id
+  getNode(id).then(actualizeNodeForm)
+}
 
 function getNodeTypes() {
   return fetch('/api/node-types')
@@ -31,6 +51,25 @@ function fillNodeTypesSelect(nodeTypes) {
   })
 }
 
+function fillNodeSelect(nodes) {
+  nodes.forEach(node => {
+    const { option } = useNodeOptionTemplate()
+    const { id, name } = node
+
+    option.value = id
+    option.textContent = `${id} \u00A0 ${name}`
+
+    nodeSelect.append(option)
+  })
+}
+
+function useNodeOptionTemplate() {
+  const { content } = nodeOptionTemplate
+  const option = content.firstElementChild.cloneNode(true)
+
+  return { option }
+}
+
 function useNodeTypeOptionTemplate() {
   const { content } = nodeTypeOptionTemplate
   const option = content.firstElementChild.cloneNode(true)
@@ -39,15 +78,17 @@ function useNodeTypeOptionTemplate() {
 }
 
 async function getCurrentNodeId() { // from hash
+  if (!nodeSelect.options.length) {
+    goTo('add-node')
+  }
+  
   let id = location.hash.slice(1)
 
-  if (!id) {
-    const nodes = await getNodes()
-    const lastNode = nodes.at(-1)
-    
-    id = lastNode.id
+  if (!id || !nodeSelect.querySelector(`option[value="${id}"]`)) {
+    id = nodeSelect.value
     location.hash = id
   }
+
   return id
 }
 
@@ -58,10 +99,18 @@ function getNode(id) {
 
 function actualizeNodeForm(node) {
   const { id, name, typeId } = node
+  const nodeOption = nodeForm.id.querySelector(`option[value="${id}"]`)
 
+  nodeForm.id.querySelector('[selected]')?.toggleAttribute('selected')
   nodeForm.id.value = id
+  nodeOption.toggleAttribute('selected')
+  nodeOption.textContent = `${id} \u00A0 ${name}`
+  nodeForm.name.setAttribute('value', name)
   nodeForm.name.value = name
+  nodeForm.type.querySelector('[selected]')?.toggleAttribute('selected')
+  nodeForm.type.setAttribute('value', typeId)
   nodeForm.type.value = typeId
+  nodeForm.type.querySelector(`option[value="${typeId}"]`)?.toggleAttribute('selected')
 }
 
 function getNodes() {
@@ -69,6 +118,114 @@ function getNodes() {
     .then(result => result.json())
 }
 
-function handleNodeFormSubmit() {
+function handleNodeFormSubmit(e) {
+  const button = e.submitter
+  const action = button.value
 
+  if (action === 'delete') {
+    deleteNode()
+  } else if (action === 'save') {
+    saveNode()
+  } else if (action == 'revert') {
+    nodeForm.reset()
+  }
+}
+
+async function deleteNode() {
+  const id = nodeForm.id.value
+  const nodeIds = [id]
+
+  const init = {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(nodeIds),
+  }
+  const result = await fetch('/api/nodes', init)
+
+  handleFirstFeedback()
+
+  if (result.ok) {
+    const [node] = await result.json()
+
+    tellNode(node, 'delete')
+    removeNodeOption(node)
+    getCurrentNodeId().then(getNode).then(actualizeNodeForm)
+  } else {
+    const problem = await result.json()
+
+    tellProblem(problem.complaint)
+  }
+}
+
+async function saveNode() {
+  const payload = Object.fromEntries(new FormData(nodeForm))
+  const init = {
+    method: 'PUT',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload),
+  }
+  const response = await fetch('/api/nodes', init)
+
+  if (response.ok) {
+    const node = await response.json()
+
+    location.hash = node.id
+    actualizeNodeForm(node)
+
+    handleFirstFeedback()
+
+    tellNode(node)
+  } else {
+    const problem = await response.json()
+
+    tellProblem(problem.complaint)
+  }
+}
+
+function tellNode(node, action = 'save') {
+  const { li, btn, a, span } = useFeedbackTemplate()
+  const children = [...li.childNodes]
+
+  btn.textContent = node.id
+  a.textContent = node.name
+  a.href += node.id
+
+  if (action === 'delete') {
+    span.textContent = 'deleted'
+  }
+
+  li.replaceChildren(...firstFeedback.childNodes)
+  restOfLines.prepend(li)
+  firstFeedback.replaceChildren(...children)
+}
+
+function removeNodeOption(node) {
+  const option = nodeSelect.querySelector(`option[value="${node.id}"]`)
+
+  option?.remove()
+}
+
+function handleFirstFeedback() {
+  if (details.hidden) {
+    details.hidden = false
+    singleFeedback.hidden = true
+    firstFeedback.replaceChildren(...singleFeedback.childNodes)
+    restOfLines.innerHTML = ''
+  }
+}
+
+function tellProblem(complaint) {
+  const { li } = useFeedbackTemplate()
+
+  li.replaceChildren(...firstFeedback.childNodes)
+  restOfLines.prepend(li)
+  firstFeedback.textContent = complaint
+}
+
+function useFeedbackTemplate() {
+  const { content } = feedbackTemplate
+  const li = content.firstElementChild.cloneNode(true)
+  const [btn, a, span] = li.children
+
+  return { li, btn, a, span }
 }
